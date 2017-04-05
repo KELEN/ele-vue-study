550e9a41ef2201a89bab64c75f4b7a26323de424
黄凯伦 <340443366@qq.com>
2017-04-05 16:44:40 +0800

    更新

--- src/components/home/HleftMenu.vue
+++ src/components/home/HleftMenu.vue
@@ -54,26 +54,31 @@
         border: 1px solid #E1E1E1;
     }
     .category-list {
-        height: 100%;
+        position: relative;
+        left: 0;
+        top: 0;
+        box-sizing: border-box;
         .category-tab {
             width: 24%;
             float: left;
+            box-sizing: border-box;
             li {
                 line-height: 50px;
+                box-sizing: border-box;
                 border-bottom: 1px solid #E1E1E1;
+                border-right: 1px solid #e1e1e1;
                 text-align: center;
             }
         }
-        .category-tab-content {
-            width: 76%;
-            float: left;
-        }
-    }
-    .category-tab-content {
-        display: none;
     }
     .active {
+        border-right: none !important;
+        background-color: white;
         display: block;
+    }
+    .category-tab-content {
+        left: 30%;
+        overflow: auto;
     }
 </style>
 <template>
@@ -83,17 +88,11 @@
                 <h3 class="side-header">全部分类</h3>
                 <div class="category-list">
                     <ul class="category-tab">
-                        <li v-on:click="loadDetail(item.id, $event)" v-for="item in categories" :data-target="'tc' + item.id">
+                        <li @click="loadDetail(item.id, $event)" v-for="item in categories" :data-target="'tc' + item.id">
                             {{ item.name }}
                         </li>
                     </ul>
-                    <ul class="category-tab-content">
-                        <li v-for="item in categories" :id="'tc' + item.id">
-                            <ul>
-                                <li>2222</li>
-                            </ul>
-                        </li>
-                    </ul>
+                    <component class="category-tab-content" :is="detailView" keep-alive></component>
                 </div>
             </aside>
             <div class="side-menu-overlay" v-on:click="closeDrawer()"></div>
@@ -101,7 +100,13 @@
     </transition>
 </template>
 <script>
+    import view8141 from '../leftMenuDetail/view8141.vue'
+    import view8142 from '../leftMenuDetail/view8142.vue'
+    import view8143 from '../leftMenuDetail/view8143.vue'
     export default {
+        components: {
+            view8141, view8142, view8143
+        },
         props: {
             open: {
                 type: Boolean,
@@ -110,7 +115,8 @@
         },
         data () {
             return {
-                categories: []
+                categories: [],
+                detailView: "view8141"
             }
         },
         computed: {
@@ -120,11 +126,12 @@
         },
         mounted () {
             let that = this;
+            // 默认推荐
             $.get('/src/json/menu-category.json', function(data, err) {
                 that.categories = data;
                 var first = data[0].id;
                 $.get('/src/json/menu-detail-' + first + ".json", function(data) {
-                    $(".category-tab-content[data-id='" + first + "']").addClass("active");
+                    $(".category-tab li[data-target=tc" + first + "]").addClass("active");
                 })
             })
         },
@@ -134,13 +141,14 @@
                 this.isOpen = false;
             },
             loadDetail(id, event) {
-                $(".category-tab li").removeClass("active");
-                var clickTarget = $(event.srcElement)
-                clickTarget.addClass("active");
-                var target = clickTarget.attr("data-target");
-                $.get("/src/json/menu-detail-" + id + ".json", function (data, err) {
-                    $("#" + target).addClass("active");
-                })
+                $(".category-tab>li").removeClass("active")
+                $(event.srcElement).addClass("active");
+                if (+id === 8142 || +id === 8141 || +id === 8143) {
+                    console.log("view" + id)
+                    this.detailView = "view" + id;
+                } else {
+                    this.detailView = "view8142";
+                }
             }
         }
     }
---
+++ src/components/leftMenuDetail/view8143.vue
@@ -0,0 +1,74 @@
+<style lang="less" scoped>
+    .category-wrap {
+        background: white;
+        margin-bottom: 200px;
+        a {
+            color: black;
+        }
+    }
+    .category-title {
+        font-size: 14px;
+        padding: 15px;
+        line-height: 14px;
+        color: #9B9B9B;
+        font-weight: 600;
+    }
+    .category-list {
+        li {
+            width: 33%;
+            float: left;
+            margin: 5px 0;
+            text-align: center;
+            img {
+                width: 70%;
+            }
+            span {
+                display: inline-block;
+            }
+        }
+        &:after {
+            content: '';
+            display: block;
+            clear: both;
+            height: 0;
+            visibility: hidden;
+         }
+    }
+    .channel-btn {
+        width: 90%;
+        margin: 0 auto;
+    }
+</style>
+<template>
+    <div class="category-wrap">
+        <div v-for="item in detail">
+            <button class="channel-btn" v-if="item.type == 'channel'">{{ item.list[0].name }}</button>
+            <div v-else>
+                <h2 class="category-title">{{ item.name }}</h2>
+                <ul class="category-list">
+                    <li v-for="i in item.list">
+                        <a href="">
+                            <img :src="i.pic" alt="">
+                            <span>{{ i.name }}</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                detail: {}
+            }
+        },
+        mounted () {
+            var that = this;
+            $.get("/src/json/menu-detail-8143.json", function(data) {
+                that.detail = data.data.mainBlock.models;
+            })
+        }
+    }
+</script>
